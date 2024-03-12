d1e91304db8fa7eaaca1710fcc56a8915a7d9078
chandanB24 <106895903+chandanB24@users.noreply.github.com>
2024-03-12 21:12:47 +0530

    pushing variables and types

--- src/Variable_and_Types/index.ts
+++ src/Variable_and_Types/index.ts
@@ -1,3 +1,16 @@
 //Basic program 
-const message:String = "Hello welcome to typescript"
+const message:string = "Hello welcome to typescript"
 console.log(message)
+//types in typescript
+//number: For numeric values.
+// string: For textual data.
+// boolean: For true/false values.
+// any: A wildcard type that can be anything.
+// arrays: For collections of values, denoted by Type[] or Array<Type>.
+const personName:string = "Chandan B"
+let age:number = 21
+const email:any = "chandanb24002@gmail.com"
+let isWorking:boolean = true
+let hobbies:string[] = ["cricket","video games","music"]
+//using const the value cannot be changed and using let the value can bechanged or reassigned to new value
+
