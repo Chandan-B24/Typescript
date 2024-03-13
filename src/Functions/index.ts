a253c116c0eea1b6dea9f365d0b68f69781376f9
chandanB24 <106895903+chandanB24@users.noreply.github.com>
2024-03-13 10:02:32 +0530

    Functions

---
+++ src/Functions/index.ts
@@ -0,0 +1,16 @@
+//Functions in typescript
+//in typescript all the functions are typed and the parameters passing to the functions are typed
+function add(a:number,b:number):number{
+    return a + b;
+}
+
+console.log(add(3,4));
+
+
+//arrow funcions in typescript
+const multiply = (a:number,b:number):number => {
+    return a * b
+}
+
+console.log(multiply(4,3))
+
