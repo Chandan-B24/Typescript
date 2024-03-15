fa8b40b047e53a942f114ed2e0991e87db1bd4e7
chandanB24 <106895903+chandanB24@users.noreply.github.com>
2024-03-15 13:12:00 +0530

    simple calculator

--- src/Modules/index.ts
+++ src/Modules/index.ts
@@ -1,6 +1,14 @@
 //exporting and importing modules
 
 import { add, sub } from "./math";
+import { Calculator } from "./calculator";
 
 console.log(add(1,2));
 console.log(sub(1,2));
+
+const calc = new Calculator()
+
+console.log(calc.add(1,2))
+console.log(calc.sub(5,2))
+console.log(calc.mul(1,2))
+console.log(calc.div(4,2))
